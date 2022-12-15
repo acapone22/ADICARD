12b5bbccae3f32d951f342a74d00910af279e839
acapone22 <113518430+acapone22@users.noreply.github.com>
2022-12-15 09:27:46 +0100

    APP ICON

--- ADICARD/cartatr.swift
+++ ADICARD/cartatr.swift
@@ -11,5 +11,5 @@ struct Carta{
     let prompt: String
     let answer: String
 
-    static let example = Carta(prompt: "Pagamento online", answer: "-33,00$")
+    static let example = Carta(prompt: "Payment ADISURC", answer: "-3,00$")
 }
